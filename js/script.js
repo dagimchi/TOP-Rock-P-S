4116171bcbc7fb4cb058faa0368e387e3eada8b3
Hae <haechoi92@yahoo.com>
2023-07-17 14:54:38 -0700

    Add function that plays a round

--- js/script.js
+++ js/script.js
@@ -5,4 +5,31 @@ function getComputerChoice() {
   else return "Scissors";
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "Rock") {
+    return "It's a tie! Rock against rock.";
+  } else if (playerSelection === "rock" && computerSelection === "Paper") {
+    return "You lose! Paper beats rock.";
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats scissors.";
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats rock.";
+  } else if (playerSelection === "paper" && computerSelection === "Paper") {
+    return "It's a tie! Paper against paper.";
+  } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+    return "You lose! Scissors beats paper.";
+  } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+    return "You lose! Rock beats scissors.";
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats paper.";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "Scissors"
+  ) {
+    return "It's a tie! Scissor against scissor.";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
