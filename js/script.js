31f23484a6bfb70587c09cfef60d576bd1418992
Hae <haechoi92@yahoo.com>
2023-07-17 17:57:56 -0700

    Create a function that plays the game 5 times and keeps score

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randNum = Math.floor(Math.random() * 3) + 1;
   if (randNum === 1) return "Rock";
@@ -9,18 +12,24 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "Rock") {
     return "It's a tie! Rock against rock.";
   } else if (playerSelection === "rock" && computerSelection === "Paper") {
+    computerScore++;
     return "You lose! Paper beats rock.";
   } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    userScore++;
     return "You Win! Rock beats scissors.";
   } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    userScore++;
     return "You Win! Paper beats rock.";
   } else if (playerSelection === "paper" && computerSelection === "Paper") {
     return "It's a tie! Paper against paper.";
   } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+    computerScore++;
     return "You lose! Scissors beats paper.";
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+    computerScore++;
     return "You lose! Rock beats scissors.";
   } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    userScore++;
     return "You Win! Scissors beats paper.";
   } else if (
     playerSelection === "scissors" &&
@@ -31,10 +40,16 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  let userScore = 0;
-  let computerScore = 0;
   for (let i = 1; i <= 5; i++) {
     let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(`Game ${i} of 5:
+            
+                You chose - ${playerSelection}
+                Your opponent chose - ${computerSelection}
+                
+                ${playRound(playerSelection, computerSelection)}
+                You - ${userScore} vs Computer - ${computerScore}`);
   }
 }
 
