93cc305b4be66ac0172c9566d6bc30c32f597875
Hae <haechoi92@yahoo.com>
2023-07-17 19:12:28 -0700

    Add Comments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let userScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
+  // RETURN RANDOM NUMBER BETWEEN 1 AND 3 AND STORE IN A VARIABLE
   let randNum = Math.floor(Math.random() * 3) + 1;
   if (randNum === 1) return "Rock";
   else if (randNum === 2) return "Paper";
@@ -40,9 +41,13 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+  // LOOP 5 TIMES THE GAME FUNCTION
   for (let i = 1; i <= 5; i++) {
+    // RETURN USER STRING VIA PROMPT IN ALL LOWER CASE AND STORE IN A VAR
     let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    // RETURN RANDOM STRING OF 3 DETERMINED POSSIBILITIES VIA GETCOMPUTERCHOICE FUNC AND STORE IN A VAR
     let computerSelection = getComputerChoice();
+    // OUTPUT RESULTS TO THE CONSOLE
     console.log(`Game ${i} of 5:
             
                 You chose - ${playerSelection}
